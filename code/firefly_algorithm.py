5ed35c8811a8b827d4efcfd1e0e3d2dde316744b
subrahmanyam tanala <subrahmanyamtanala@gmail.com>
2026-03-09 21:51:44 +0530

    Create firefly_algorithm.py with Firefly class and FireflyAlgorithm implementation

---
+++ code/firefly_algorithm.py
@@ -0,0 +1,22 @@
+class Firefly:
+    def __init__(self, position, attractiveness):
+        self.position = position
+        self.attractiveness = attractiveness
+
+    def move_towards(self, other_firefly):
+        # Logic to move towards another firefly based on attractiveness
+        pass
+
+class FireflyAlgorithm:
+    def __init__(self, num_fireflies, num_iterations):
+        self.num_fireflies = num_fireflies
+        self.num_iterations = num_iterations
+        self.fireflies = []
+
+    def initialize_fireflies(self):
+        # Logic to initialize fireflies
+        pass
+
+    def run(self):
+        # Logic to run the firefly algorithm
+        pass
